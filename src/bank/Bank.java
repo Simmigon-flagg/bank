56204194f0b9cc3fa7c56d6182fe86eb02b81e95
Simmigon-flagg <simmigon.flagg@gmail.com>
2015-04-04 01:09:42 -0400

    First Commit

---
+++ src/bank/Bank.java
@@ -0,0 +1,34 @@
+/**
+ * Explain the purpose of the program as detail as possible - 13%.
+ *
+ * Develop asolution for the problem and mention algorithms to be used - 16%.
+ * 
+ * The Data structures we will use is Queues and ArrayList. ---Done
+ *
+ * Give a description of how to use the program and expected input/output - 5%.
+ *
+ * Explain the purpose of each class you develop in the program.
+ * The purpose of the 
+ *
+ */
+package bank;
+
+/**
+ *
+ * @author Flagg
+ */
+public class Bank {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Customers {
+
+}
+
+class Teller {
+
+}
